aa05c51a020c670005ba6e6cb6b925986b9a347c
David Herrera Caicedo <david18hc@gmail.com>
2020-10-25 12:27:37 -0500

    Primera versión corregida Layouts Usuario

---
+++ uPet/app/src/main/java/com/example/proyectoupet/UsuarioAdministrarPaseosActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoupet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioAdministrarPaseosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_administrar_paseos);
+    }
+}
---
+++ uPet/app/src/main/java/com/example/proyectoupet/UsuarioSeleccionarMascotasActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoupet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioSeleccionarMascotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_seleccionar_mascotas);
+    }
+}
