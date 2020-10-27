7d7e0c5b020c83c33ac52cd31ac8e8ba1b560d69
Daniel Fernandez <dafeul@hotmail.com>
2020-10-27 12:30:30 -0500

    Cuadro de dialogo para borrar

--- uPet/app/src/main/java/com/example/proyectoupet/PerfilMascota.java
+++ uPet/app/src/main/java/com/example/proyectoupet/PerfilMascota.java
@@ -2,11 +2,14 @@ package com.example.proyectoupet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +22,7 @@ public class PerfilMascota extends AppCompatActivity {
     Button botoneditar;
     Button botonborrar;
     ImageView imagen_mascota;
+    AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,24 @@ public class PerfilMascota extends AppCompatActivity {
         botonborrar =  findViewById(R.id.botonBorrarMascota);
         imagen_mascota = findViewById(R.id.showImagenMascota);
 
+        nombreMascota.setText("Manchas");
+        edad.setText("5");
+        especie.setText("Perro");
+        raza.setText("Golden Retriever");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Borrar Mascota");
+        builder.setMessage("¿Esta seguro que quiere eliminar la mascota?");
+
+        builder.setPositiveButton("Borrar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "Borrado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+        dialog = builder.create();
+
         botoneditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +65,7 @@ public class PerfilMascota extends AppCompatActivity {
         botonborrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "Borrado", Toast.LENGTH_SHORT).show();
+                dialog.show();
             }
         });
     }
