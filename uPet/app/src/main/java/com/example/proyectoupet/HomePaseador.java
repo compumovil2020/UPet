5ea3e88a2c0959e5a175a3ef5a0c703f111e03c5
randy <randydarrell45@hotmail.com>
2020-10-28 00:09:46 -0500

    se agrega menu auxliar en el toolbar del home

--- uPet/app/src/main/java/com/example/proyectoupet/HomePaseador.java
+++ uPet/app/src/main/java/com/example/proyectoupet/HomePaseador.java
@@ -1,10 +1,15 @@
 package com.example.proyectoupet;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,12 +17,32 @@ import com.example.proyectoupet.paseos.CrearPaseo;
 
 public class HomePaseador extends AppCompatActivity {
 
+    private DrawerLayout sideBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_paseador);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.logout){
+            System.out.println("salio");
+            ///aqui agregas tu logica
+        }
+        return true;
     }
 
     public void toCrearPaseo(View v){
--- uPet/app/src/main/java/com/example/proyectoupet/HomeUsuarioActivity.java
+++ uPet/app/src/main/java/com/example/proyectoupet/HomeUsuarioActivity.java
@@ -1,10 +1,14 @@
 package com.example.proyectoupet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -14,11 +18,15 @@ public class HomeUsuarioActivity extends AppCompatActivity {
     private ImageButton btnAdminPaseos;
     private ImageButton btnSitiosInteres;
     private ImageButton btnAdminMascotas;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_usuario);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         btnPerfil =findViewById(R.id.btnPerfilUsuario);
         btnAdminPaseos = findViewById(R.id.btnAdminPaseosUsuario);
         btnSitiosInteres = findViewById(R.id.btnSitiosInteresUsuario);
@@ -53,5 +61,21 @@ public class HomeUsuarioActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.logout){
+            System.out.println("salio");
+            ///aqui agregas tu logica
+        }
+        return true;
     }
 }
