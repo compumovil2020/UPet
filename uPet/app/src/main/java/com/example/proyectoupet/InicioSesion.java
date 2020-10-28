43ebb531fcb0c8fa71fcfa02ce2cece1270dac81
Sergio <sposada16@gmail.com>
2020-10-27 23:35:57 -0500

    Primera Parte Autenticación

--- uPet/app/src/main/java/com/example/proyectoupet/InicioSesion.java
+++ uPet/app/src/main/java/com/example/proyectoupet/InicioSesion.java
@@ -1,16 +1,29 @@
 package com.example.proyectoupet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class InicioSesion extends AppCompatActivity {
+    public static final String TAG = "ProyectoUpet";
 
-    Button botLogin;
-    Button botRegistro;
+
+    Button botLogin, botRegistro;
+    EditText username, password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +32,14 @@ public class InicioSesion extends AppCompatActivity {
 
         botRegistro = findViewById(R.id.boton_siguiente);
         botLogin = findViewById(R.id.boton_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
 
         botLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), HomeUsuarioActivity.class));
+                attemptSignin();
             }
         });
 
@@ -34,4 +50,49 @@ public class InicioSesion extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        updateUI(user);
+    }
+
+    private void updateUI(FirebaseUser user){
+        if(user != null){
+            startActivity(new Intent(this, HomeUsuarioActivity.class));
+        }else{
+            username.setText("");
+            password.setText("");
+            //limpiar el formulario
+        }
+    }
+
+    private void attemptSignin(){
+        String email = username.getText().toString();
+        String pass = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(InicioSesion.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
